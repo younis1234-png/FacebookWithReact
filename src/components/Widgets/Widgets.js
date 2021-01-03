41dd80bd48d324144eee5f64e8db26a35769e01c
Younis saleh <younissaleh@Youniss-MacBook-Pro.local>
2021-01-03 13:42:48 -0500

    first commit

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -5,7 +5,7 @@ function Widgets() {
     return (
     <div className="widgets">
         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTop-Media-Growth-1805926782992492%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" 
-                width="340"
+                width="330"
                 height="100%"
                 style={{ border:'none', overflow: 'hidden' }}
                 scrolling="no"
