c819815f8264bcf0974878fab4feda2b57d1d0f9
Younis <younis@Youniss-MacBook-Pro.local>
2021-05-04 10:26:46 -0400

    firbase connection

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,24 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase"
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiC6KoHOxcB1nx3ydRg2uT46PnVeSjlAg",
-    authDomain: "facebook-clone-e1cc5.firebaseapp.com",
-    projectId: "facebook-clone-e1cc5",
-    storageBucket: "facebook-clone-e1cc5.appspot.com",
-    messagingSenderId: "839319178320",
-    appId: "1:839319178320:web:7ecdd6e88e3078a7d99555",
-    measurementId: "G-3P31NLZDGE"
+  apiKey: "AIzaSyAD9f-slcIUjPVxjmFUlJwUDvJIsWD7TiI",
+  authDomain: "facebook-198e8.firebaseapp.com",
+  projectId: "facebook-198e8",
+  storageBucket: "facebook-198e8.appspot.com",
+  messagingSenderId: "99224719818",
+  appId: "1:99224719818:web:97518f85d11b9ca0b2ff33",
+  measurementId: "G-QRP84MJ7T7",
 };
 
-// connect our front to our backend 
-const firebaseApp = firebase.initializeApp( firebaseConfig );
+// connect our front to our backend
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-// Get access to database 
+// Get access to database
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-// Set up our provider, i tell firbase we want the google login 
+// Set up our provider, i tell firbase we want the google login
 const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider };
